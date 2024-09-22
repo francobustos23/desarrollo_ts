a26a8583af0c61061a899ca65a7a152850b23cc3
francobustos23 <bustosfranco071@gmail.com>
2024-09-22 12:10:24 -0300

    services

--- src/services/User.ts
+++ src/services/User.ts
@@ -1,10 +1,11 @@
 import { Model } from "sequelize";
 import User from "../Models/User";
 
-interface IUser {
+type IUser = {
     name: string;
     email: string;
     password: string;
+    role: string;
 }
 
 class UserService {
@@ -14,7 +15,7 @@ class UserService {
         return await User.findAll();
     }
 
-    async findOneUser(id: number): Promise<Model<any, any> | null> {
+    async findOneUser(id: string): Promise<Model<any, any> | null> {
         return await User.findByPk(id);
     }
 
