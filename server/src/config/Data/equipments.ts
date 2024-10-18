5a5f81ffcfe79ff2991f9676939c2c4db717e2fd
francobustos23 <bustosfranco071@gmail.com>
2024-10-18 20:54:27 -0300

    equipments data

---
+++ server/src/config/Data/equipments.ts
@@ -0,0 +1,31 @@
+export const equipmentData = [
+    {
+      name: "Notebook HP Pavilion",
+      description: "Laptop con procesador Intel i5 y 8GB de RAM",
+      stock: 10,
+      state: true,
+      categoryName: "notebooks",
+      
+    },
+    {
+      name: "Impresora HP LaserJet",
+      description: "Impresora láser multifunción",
+      stock: 7,
+      state: true,
+      categoryName: "impresoras",
+    },
+    {
+      name: "Teclado Logitech K120",
+      description: "Teclado de membrana con cable USB",
+      stock: 30,
+      state: true,
+      categoryName: "teclados",
+    },
+    {
+      name: "Mouse Logitech G502",
+      description: "Mouse gamer con sensor de alta precisión",
+      stock: 20,
+      state: true,
+      categoryName: "mouses",
+    }
+  ];
