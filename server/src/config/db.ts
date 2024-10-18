b000f3391e6cd1e95efbdf5abb26c127cca5429a
francobustos23 <bustosfranco071@gmail.com>
2024-10-18 20:57:15 -0300

    sequelize instance

---
+++ server/src/config/db.ts
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize-typescript";
+import { envs } from "../environments/environments";
+import { User, Equipment, Category, Movement } from '../Models/index'
+
+const {
+    DB_DIALECT,
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USER
+} = envs;
+
+const db = new Sequelize({
+    database: DB_NAME,
+    dialect: DB_DIALECT,
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    models: [
+        User,
+        Equipment,
+        Category,
+        Movement
+    ]
+});
+
+export default db;
