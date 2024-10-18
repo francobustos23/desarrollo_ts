277481775e40432f3ccd7fb87189d3652002b9fa
francobustos23 <bustosfranco071@gmail.com>
2024-10-18 20:53:56 -0300

    categories data

---
+++ server/src/config/Data/categories.ts
@@ -0,0 +1,19 @@
+export const categoriesData = [
+    {
+      name: "notebooks",
+      description:
+        "Computadoras portátiles de diversas marcas y especificaciones.",
+    },
+    {
+      name: "impresoras",
+      description: "Dispositivos para imprimir documentos y gráficos en papel.",
+    },
+    {
+      name: "teclados",
+      description: "Dispositivos de entrada para introducir texto y comandos.",
+    },
+    {
+      name: "mouses",
+      description: "Dispositivos de entrada para navegación y selección.",
+    }
+  ];
