cf9f463fd8502f1008c56d3385c8eb9b75e11fc2
francobustos23 <bustosfranco071@gmail.com>
2024-10-18 21:01:21 -0300

    index export models

---
+++ server/src/Models/index.ts
@@ -0,0 +1,6 @@
+import User from './User';
+import Equipment from './Equipment';
+import Category from './Categories';
+import Movement from './Movements';
+
+export { User, Equipment, Category, Movement};
