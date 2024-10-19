73f640e040cd2baca3c32bbf8dd152ca7bc36671
francobustos23 <bustosfranco071@gmail.com>
2024-10-19 14:24:21 -0300

    interfaces

---
+++ client/src/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface Product {
+    id: string; 
+    name: string;
+    categoryId: string;
+    stock: number; 
+  }
+  
+export interface DataItem {
+    id: string; 
+    name: string;
+    stock: number;
+    categoryId: string;
+  }
+  
