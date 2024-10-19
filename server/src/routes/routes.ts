83edad65e9c52dfc57e5bcfee7987c454b887b2a
francobustos23 <bustosfranco071@gmail.com>
2024-10-19 14:23:53 -0300

    a new route has been added

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import userRoutes from './user.routes';
 import movementRoutes from './movement.routes'
+import equipmentRoutes from './equipment.routes';
 import authRoutes from './auth.routes';
 
 const router: Router = Router();
@@ -8,5 +9,6 @@ const router: Router = Router();
 router.use(userRoutes);
 router.use(movementRoutes);
 router.use(authRoutes);
+router.use(equipmentRoutes);
 
 export { router };
