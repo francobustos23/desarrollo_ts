304a716f3d6d0ea9ff2c39610b5d04094fd26078
francobustos23 <bustosfranco071@gmail.com>
2024-10-18 21:04:27 -0300

    server file

---
+++ server/src/Server.ts
@@ -0,0 +1,45 @@
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import express, { Application } from 'express';
+import { envs } from './environments/environments';
+import { connectDB } from './config/connectDB';
+import { router } from './routes/routes';
+
+export class mServer {
+    private app: Application;
+    private port: number;
+
+    constructor(){
+        this.app = express();
+        this.port = envs.PORT;
+
+        this.connectDB();
+        this.middleweares();
+        this.routes();
+
+    }
+
+    private middleweares():void {
+        this.app.use(cors());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'))
+        this.app.use(express.json())
+    }
+
+    private routes(): void {
+        this.app.use('/api', router);
+    }
+
+    private async connectDB(): Promise<void> {
+        await connectDB();
+    }
+
+    public listen(): void {
+        this.app.listen(this.port, () => {
+            console.log(`server runnig on http://localhost:${this.port}`);
+        })
+    }
+}
+
+export default mServer;
