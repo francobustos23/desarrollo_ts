e06030e9ef7cffb9d21ab84ec0b0f057c40c7867
francobustos23 <bustosfranco071@gmail.com>
2024-10-18 21:01:40 -0300

    equipment model

---
+++ server/src/Models/Equipment.ts
@@ -0,0 +1,47 @@
+import { Table, Column, Model, DataType, HasMany, ForeignKey, BelongsTo, PrimaryKey, Default } from "sequelize-typescript";
+import {User, Category} from "./index"
+import {v4 as uuidv4} from "uuid";
+
+@Table({
+    timestamps: false,
+    tableName: "equipments",
+})
+
+class Equipment extends Model {
+    @Default(uuidv4)
+    @PrimaryKey
+    @Column({
+        type: DataType.UUID,
+        allowNull: false
+    })
+    id!: string;
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    name!: string;
+    @Column({
+        type: DataType.STRING,
+        defaultValue: true,
+    })
+    state!: string;
+    @ForeignKey(() => Category)
+    categoryId!: string;
+
+    @BelongsTo(() => Category)
+    category!: Category;
+    @Column({
+        type: DataType.DATE,
+        defaultValue: DataType.NOW,
+    })
+    createdAt!: Date;
+    @Column({
+        type: DataType.DATE,
+        defaultValue: DataType.NOW,
+    })
+    updatedAt!: Date;
+
+
+}
+
+export default Equipment;
