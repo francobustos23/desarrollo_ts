163f61ca4457f5516d6de9614edb34e2e3acf06a
francobustos23 <bustosfranco071@gmail.com>
2024-10-20 17:31:23 -0300

    equipment routes modified

--- server/src/routes/equipment.routes.ts
+++ server/src/routes/equipment.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createEquipment, getEquipments } from "../controllers/equipment.controller";
+import { createEquipment, getEquipments, updateEquipment } from "../controllers/equipment.controller";
 
 
 const router: Router = Router();
 
 router.get('/all/equipments', getEquipments);
 router.post('/equipment', createEquipment);
+router.put('/equipment/:id', updateEquipment)
 
 export default router;
