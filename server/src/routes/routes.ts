1cac1398243ef9c7ce37b4d209f1b89cefddee9d
francobustos23 <bustosfranco071@gmail.com>
2024-10-20 17:31:34 -0300

    index routes modified

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import userRoutes from './user.routes';
-import movementRoutes from './movement.routes'
 import equipmentRoutes from './equipment.routes';
 import authRoutes from './auth.routes';
+import categoryRoutes from './category.routes'
 
 const router: Router = Router();
 
 router.use(userRoutes);
-router.use(movementRoutes);
 router.use(authRoutes);
 router.use(equipmentRoutes);
+router.use(categoryRoutes);
 
 export { router };
