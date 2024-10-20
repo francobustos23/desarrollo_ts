df528c2938a88b9c765b1c771f564052bc73e134
francobustos23 <bustosfranco071@gmail.com>
2024-10-20 17:22:46 -0300

    CRUD functionalities of computer equipment and alerts have been added

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -2,26 +2,45 @@ import { useEffect, useState } from 'react';
 import { Trash2, Edit, Save, X } from 'lucide-react';
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
-import { DataItem, Product } from '../../interfaces';
+import { Table, TableBody, TableCell, TableRow } from "../ui/table";
+import { DataCategory, DataItem, Product } from '../../interfaces';
 import axios from 'axios';
+import { TableHeaderComponent } from './TableHeader.component';
 
+import Swal from 'sweetalert2'
 
 
 export const Landing = (): JSX.Element => {
-  const [data, setData] = useState<DataItem[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+
+  const [data, setData] = useState<DataItem[]>([]); //estado para el get de los equipos 
+  const [loading, setLoading] = useState<boolean>(true); //estado para la carga de datos
   const [error, setError] = useState<string | null>(null);
-
-  const [newProductName, setNewProductName] = useState(''); 
-  const [editingProduct, setEditingProduct] = useState<Product | null>(null); 
-
   
+  //crear y editar equipos
+  const [newProductName, setNewProductName] = useState('');
+  const [editingProduct, setEditingProduct] = useState<Product | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [inputStock, setInputStock] = useState<number>(0);
+  
+  //cargar categorias al montar el componente
+  const [dataCategory, setDataCategory] = useState<DataCategory[]>([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/all/categories');
+        setDataCategory(response.data);
+      } catch (error) {
+        console.log('Error al traer las categorias ', error)
+      }
+    }
+    fetchCategories();
+  }, [])
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get<DataItem[]>('http://localhost:3000/api/all/equipments');
-        setData(response.data);
+        setData(response.data.filter(item => item.state));
         setLoading(false);
       } catch (error: any) {
         setError(error.message);
@@ -33,15 +52,30 @@ export const Landing = (): JSX.Element => {
 
   // Función para agregar un nuevo producto
   const addProduct = async () => {
+    if (selectedCategory === '') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Debes seleccionar una categoría',
+        timer: 2500
+      })
+      return;
+    }
     if (newProductName.trim() !== '') {
       try {
         const response = await axios.post('http://localhost:3000/api/equipment', {
           name: newProductName,
-          stock: 0, 
-          categoryId: "1bc3d7b9-321f-4417-9960-0a0708f58076" 
+          stock: inputStock,
+          categoryId: selectedCategory
         });
         setData([...data, response.data]);
-        setNewProductName(''); 
+        setNewProductName('');
+        setSelectedCategory('');
+        Swal.fire({
+          icon: 'success',
+          title: 'Producto agregado',
+          timer: 1500
+        })
       } catch (error) {
         setError('Error al agregar el producto');
       }
@@ -53,35 +87,40 @@ export const Landing = (): JSX.Element => {
     setEditingProduct({ ...product });
   };
 
-  // Función para cancelar la edición de un producto
+  // Función para cancelar la edición de un equipo
   const cancelEditing = () => {
     setEditingProduct(null);
   };
 
-  // Función para guardar los cambios en un producto
+  // Función para guardar los cambios en un equipo
   const saveEdit = async () => {
     if (editingProduct) {
       try {
-        await axios.put(`http://localhost:3000/api/equipments/${editingProduct.id}`, {
+        await axios.put(`http://localhost:3000/api/equipment/${editingProduct.id}`, {
           name: editingProduct.name,
           stock: editingProduct.stock,
-          categoryId: "uuid-de-la-categoria" // Asegúrate de tener el ID de categoría actualizado
+          categoryId: editingProduct.categoryId 
         });
-        setData(data.map(product => 
-          product.id === editingProduct.id ? { ...product, name: editingProduct.name, stock: editingProduct.stock } : product
+        setData(data.map(product =>
+          product.id === editingProduct.id ? { ...product, name: editingProduct.name, stock: editingProduct.stock, categoryId: editingProduct.categoryId } : product
         ));
-        setEditingProduct(null); // Limpiar la edición
+        setEditingProduct(null); 
       } catch (error) {
         setError('Error al guardar el producto');
       }
     }
   };
 
-  // Función para eliminar (o desactivar) un producto
+  // Función para eliminar un equipo
   const removeProduct = async (id: string) => {
     try {
-      await axios.put(`http://localhost:3000/api/equipments/${id}`, { state: false }); // Actualiza el estado a false en lugar de eliminar
+      await axios.put(`http://localhost:3000/api/equipment/${id}`, { state: false }); 
       setData(data.filter(product => product.id !== id));
+      Swal.fire({
+        icon: 'success',
+        title: 'Producto eliminado',
+        timer: 1500
+      })
     } catch (error) {
       setError('Error al eliminar el producto');
     }
@@ -98,8 +137,7 @@ export const Landing = (): JSX.Element => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Control de Inventario</h1>
-      
-      {/* Input para agregar un nuevo producto */}
+
       <div className="mb-4 flex gap-2">
         <Input
           type="text"
@@ -108,22 +146,33 @@ export const Landing = (): JSX.Element => {
           placeholder="Nombre del nuevo producto"
           className="max-w-sm"
         />
-        <select name="category" id="category">
-          {/* <option value={}></option> */}
+        <select 
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          <option value="">Selecciona una categoría</option>
+          {dataCategory.map((category: DataCategory) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
         </select>
+
+        <input 
+        type="number"
+        value={inputStock}
+        onChange={(e) => setInputStock(Number(e.target.value))}
+        placeholder=''
+        style={{width: "14%", marginLeft: 10}}
+        />
         <Button onClick={addProduct}>Agregar Producto</Button>
       </div>
 
-      {/* Tabla de productos */}
       <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Producto</TableHead>
-            <TableHead>Categoria</TableHead>
-            <TableHead>Cantidad</TableHead>
-            <TableHead>Acciones</TableHead>
-          </TableRow>
-        </TableHeader>
+
+        {/* Header Component */}
+        <TableHeaderComponent/>
+        
         <TableBody>
           {data.map((product) => (
             <TableRow key={product.id}>
